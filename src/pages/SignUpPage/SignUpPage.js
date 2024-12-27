2b76e8f3dc197d3a19518fe396f0097e889d5851
pankaj_gupta <pankaj_gupta@PankajGupta.cms.co.in>
2024-12-27 11:10:22 +0530

    Modify some rules for css and also implemented SAP user details page for web with responsive

--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -1,3 +1,4 @@
+import SignUpDetailsCard from '../../features/SignUpForm/SignUpDetailsCard';
 import SignupForm from '../../features/SignUpForm/SignUpForm'
 import './SignUpPage.scss';
 
@@ -8,6 +9,7 @@ function SignUpPage(){
        <div className="left-section "><p className="signUpTitle">Welcome to<span className="head-black block">DataLake 3.0</span></p></div>
        <div className="right-section  mt-32">
         <SignupForm/>
+        {/* <SignUpDetailsCard/> */}
        </div>
     </div>
 </div>
